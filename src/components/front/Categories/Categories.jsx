d2b894a722041f209b4bab45ea0cf8b16856a367
sifeddinehadi22@gmail.com <sifeddinehadi22@gmail.com>
2024-11-13 17:36:28 +0100

    [#] adding react-loading-skeleton: Home, Categories.jsx

--- src/components/front/Categories/Categories.jsx
+++ src/components/front/Categories/Categories.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 function Categories() {
 
     // On utilise cette variable pour stocker les catégories disponibles sur la plateforme.
     const [categories, setCategories] = useState([]);
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
 
@@ -17,6 +20,7 @@ function Categories() {
             }
         }).then((response) => {
             setCategories(response.data.categories);
+            setLoading(false)
         }).catch((error) => {
             console.log(error);
         })
@@ -25,13 +29,29 @@ function Categories() {
     return (
         <div className="widget bg-dark p-4 text-center">
             <h2 className="widget-title text-white d-inline-block mt-4">Catégories</h2>
-            <div className="form-group mt-4 categories">
-                {categories.map((category) => (
-                    <Link to={`/${category.slug}`} key={category.id} className="btn btn-dark">
-                        {category.name}
-                    </Link>
-                ))}
-            </div>
+            {
+                loading ? (
+                    <div 
+                    style={{
+                        marginTop: '2rem',
+                      }}
+                    >
+                        <SkeletonTheme baseColor="#202020" highlightColor="#E4112F">
+                            <p>
+                                <Skeleton count={4} />
+                            </p>
+                        </SkeletonTheme>
+                    </div>
+
+                ) : 
+                    <div className="form-group mt-4 categories">
+                        {categories.map((category) => (
+                            <Link to={`/${category.slug}`} key={category.id} className="btn btn-dark">
+                                {category.name}
+                            </Link>
+                        ))}
+                    </div>
+            }
         </div>
     )
 }
--- src/components/front/Home/Home.jsx
+++ src/components/front/Home/Home.jsx
@@ -8,6 +8,8 @@ import Founder from '../Founder/Founder'
 import Categories from '../Categories/Categories'
 import AOS from 'aos';
 import "aos/dist/aos.css"
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 
 
@@ -20,6 +22,9 @@ function Home() {
     const [info, setInfo] = useState({});
 
     const url = `${process.env.REACT_APP_BASE_URL_WITH_API}/get-posts`
+
+    const [loading, setLoading] = useState(true)
+
 
     // On utilise cette fonction pour récupérer les postes.
     const getPosts = (url) => {
@@ -34,6 +39,7 @@ function Home() {
             setPosts(response.data.posts.data);
             setInfo(response.data.posts)
             console.log(response.data.posts);
+            setLoading(false)
         }).catch((error) => {
             console.log(error);
         });
@@ -85,39 +91,54 @@ function Home() {
                     <div className="col-lg-8">
                         <h1 className='mb-5 text-white lato'>La Plume Curieuse</h1>
                         {/* On affiche tous les posts récupérés par la fonction getPosts */}
-                        {posts && posts.map((post) => (
-                            <div className="card post-item bg-transparent border-0 mb-3" key={post.id} data-aos="zoom-out">
-                                <Link to={`/${post.categories[0].slug}/${post.slug}`}>
-                                    <img className="card-img-top rounded-0" src={`${process.env.REACT_APP_BASE_URL_WITHOUT_API}/uploads/posts/picture/${post.picture}`} alt="" />
-                                </Link>
-                                <div className="card-body px-0">
-                                    <ul className="post-meta mb-2">
-                                        <li className="d-inline-block mr-3">
-                                            <span className="fas fa-clock text-primary"></span>
-                                            <a className="ml-1" href="#">{new Date(post.created_at).toLocaleDateString('fr-FR', {
-                                                year: 'numeric',
-                                                month: 'long',
-                                                day: 'numeric',
-                                                hour: 'numeric',
-                                                minute: 'numeric'
-                                            })}</a>
-                                        </li>
-                                        <li className="d-inline-block mr-3">
-                                            <span className="fas fa-list-alt text-primary"></span>
-                                            <Link className="ml-1" to={`/${post.categories[0].slug}`}>{post.categories[0].name}</Link>
-                                        </li>
-                                        <li className="d-inline-block mr-3">
-                                            <span className="fas fa-eye text-primary"></span>
-                                            <span className="ml-1 text-white">{post.views}</span>
-                                        </li>
-                                    </ul>
-                                    <h2 className="card-title">
-                                        <Link className="text-white opacity-75-onHover" to={`/${post.categories[0].slug}/${post.slug}`}>{post.title.slice(0, 50)}...</Link>
-                                        <p className="span">{post.summary.slice(0, 100)}...</p>
-                                    </h2>
-                                </div>
+                        {loading ?
+                            <div
+                                style={{
+                                    marginTop: '2rem',
+                                }}
+                            >
+                                <SkeletonTheme baseColor="#202020" highlightColor="#E4112F">
+                                    <p>
+                                        <Skeleton count={10} />
+                                    </p>
+                                </SkeletonTheme>
                             </div>
-                        ))}
+                            : (
+                            posts && posts.map((post) => (
+                                <div className="card post-item bg-transparent border-0 mb-3" key={post.id} data-aos="zoom-out">
+                                    <Link to={`/${post.categories[0].slug}/${post.slug}`}>
+                                        <img className="card-img-top rounded-0" src={`${process.env.REACT_APP_BASE_URL_WITHOUT_API}/uploads/posts/picture/${post.picture}`} alt="" />
+                                    </Link>
+                                    <div className="card-body px-0">
+                                        <ul className="post-meta mb-2">
+                                            <li className="d-inline-block mr-3">
+                                                <span className="fas fa-clock text-primary"></span>
+                                                <a className="ml-1" href="#">{new Date(post.created_at).toLocaleDateString('fr-FR', {
+                                                    year: 'numeric',
+                                                    month: 'long',
+                                                    day: 'numeric',
+                                                    hour: 'numeric',
+                                                    minute: 'numeric'
+                                                })}</a>
+                                            </li>
+                                            <li className="d-inline-block mr-3">
+                                                <span className="fas fa-list-alt text-primary"></span>
+                                                <Link className="ml-1" to={`/${post.categories[0].slug}`}>{post.categories[0].name}</Link>
+                                            </li>
+                                            <li className="d-inline-block mr-3">
+                                                <span className="fas fa-eye text-primary"></span>
+                                                <span className="ml-1 text-white">{post.views}</span>
+                                            </li>
+                                        </ul>
+                                        <h2 className="card-title">
+                                            <Link className="text-white opacity-75-onHover" to={`/${post.categories[0].slug}/${post.slug}`}>{post.title.slice(0, 50)}...</Link>
+                                            <p className="span">{post.summary.slice(0, 100)}...</p>
+                                        </h2>
+                                    </div>
+                                </div>
+                            )))
+                        }
+
                         {/* On affiche conditionnellement les boutons de pagination */}
                         <ul className='pagination justify-content-centre mb-5'>
                             {info.prev_page_url ? (
