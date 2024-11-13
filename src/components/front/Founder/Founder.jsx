11012d9d4a1912512fad267b11a18c9707ff872b
sifeddinehadi22@gmail.com <sifeddinehadi22@gmail.com>
2024-11-13 13:53:41 +0100

    [#] update Founder.jsx content

--- src/components/front/Founder/Founder.jsx
+++ src/components/front/Founder/Founder.jsx
@@ -6,7 +6,7 @@ function Founder() {
             <img className="author-thumb-sm rounded-circle d-block mx-auto" src="/images/profile.jpg"
                 alt="" />
             <h2 className="widget-title text-white d-inline-block mt-4">Le créateur</h2>
-            <p className="mt-4">Sif eddine HADI, LPC est votre plume curieuse, vous pouvez trouver tout ce qui vous intéresse, c'est l'endroit idéal pour partager nos idées dans tous les domaines.
+            <p className="mt-4">Sif eddine HADI, vous pouvez trouver tout ce qui vous intéresse, c'est l'endroit idéal pour partager nos idées dans tous les domaines.
             </p>
             <ul className="list-inline mt-3">
                 <li className="list-inline-item">
